dcdbc2bf4bf7caa60648ed91f2d6ad447c2d15ed
Ritesh Dafale <ritesh.dafale13@gmail.com>
2024-08-22 19:48:37 +0530

    button change

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -7,10 +7,10 @@ function Navbar() {
       <nav class="navbar navbar-expand-lg bg-dark">
         <div class="container-fluid">
           <a class="navbar-brand text-white" href="#">RD</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler btnColor border" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div class="collapse navbar-collapse " id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 navul">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
