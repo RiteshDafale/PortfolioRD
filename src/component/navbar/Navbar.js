65c71758e49dd1be98d0dfa2859383331735c625
Ritesh Dafale <ritesh.dafale13@gmail.com>
2024-08-28 10:20:20 +0530

    favicon and icon added

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand text-white" href="#">RD</a>
+      <a class="navbar-brand text-white logo" href="#">RD</a>
       <button class="navbar-toggler btnColor border" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
