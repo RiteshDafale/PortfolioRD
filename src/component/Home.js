924da12708417b84c23037522e7f3d6dfd6a60f4
RiteshDafale <96180597+RiteshDafale@users.noreply.github.com>
2026-02-14 17:02:21 +0530

    Add DatabricksDashboard initialization
    Integrate DatabricksDashboard into Home component.

--- src/component/Home.js
+++ src/component/Home.js
@@ -10,6 +10,17 @@ function Home() {
   return (
     <div>
       <Navbar />
+    import { DatabricksDashboard } from "@databricks/aibi-client";
+  
+const dashboard = new DatabricksDashboard({
+  instanceUrl: "https://dbc-3a0d0c45-bb5d.cloud.databricks.com",
+  workspaceId: "88284658625834",
+  dashboardId: "01f10994ad6e1a3d97c76980d86186f5",
+  token: "<<CREATED_BY_YOUR_SERVER>>", // This token should be minted by your server
+  container: document.getElementById("dashboard-container"),
+});
+
+dashboard.initialize();
       <First />
       <Footer />     
     </div>
